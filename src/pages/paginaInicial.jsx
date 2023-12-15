38965d2f589980f32512706cf74e2e9d4932dd0a
Rociogm27 <rociogoomez27@uma.es>
2023-12-15 16:08:53 +0100

    edit

--- src/pages/paginaInicial.jsx
+++ src/pages/paginaInicial.jsx
@@ -4,23 +4,37 @@ import NavbarPage from "../navbar/navbar.js";
 import CompFoto from "../Componentes/foto.js"
 
 const PaginaInicial = () => {
-
-    return(
+    const styles = {
+        welcomeBox: {
+          backgroundColor: '#dddddd', // Fondo gris claro
+          padding: '20px', // Espaciado interno
+          borderRadius: '10px', // Borde redondeado
+          width: 'fit-content', // Ajusta el ancho al contenido
+          margin: '20px auto', // Centra el cuadro horizontalmente y agrega un margen superior e inferior
+        },
+      };
+      
+    return (
         <div>
-            <NavbarPage></NavbarPage>
-            <div className="container-fluid" style={{marginLeft: '9.5vmin'}}>
-                <div className="row">
-                    <div className="col">
-                        {localStorage.getItem('objetoToken') != undefined ? 
-                        <h2> Bienvenido {JSON.parse(localStorage.getItem('objetoToken')).correo}</h2> :
-                        <h2> Bienvenido, usuario no registrado</h2>
-                        }
-                    </div>
-                </div>
+          <NavbarPage />
+          <div className="container-fluid" style={{ marginLeft: '9.5vmin' }}>
+            <div className="row">
+              <div className="col">
+                {localStorage.getItem('objetoToken') != undefined ? (
+                  <div style={styles.welcomeBox}>
+                    <h2>Bienvenido {JSON.parse(localStorage.getItem('objetoToken')).correo}</h2>
+                  </div>
+                ) : (
+                  <div style={styles.welcomeBox}>
+                    <h2>Bienvenido, usuario no registrado</h2>
+                  </div>
+                )}
+              </div>
             </div>
+          </div>
         </div>
-        
-    )
+      );
+      
 }
 
 export default PaginaInicial
